fd1bf532f596b079d32fd33096b958829a8ed383
deoxxa <deoxxa@fknsrs.biz>
2013-04-24 08:23:48 +1000

    code import

---
+++ example.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+var flv = require("./"),
+    fs = require("fs"),
+    stream = require("stream");
+
+var parser = new flv.Parser(),
+    serialiser = new flv.Serialiser();
+
+parser.on("readable", function() {
+  var e;
+  while (e = parser.read()) {
+    console.log(e);
+  }
+});
+
+var nowhere = new stream.Writable();
+nowhere._write = function _write(input, encoding, done) { return done(); };
+
+fs.createReadStream(process.argv[2]).pipe(parser).pipe(serialiser).pipe(nowhere);
